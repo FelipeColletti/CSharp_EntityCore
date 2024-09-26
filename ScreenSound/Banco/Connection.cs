7b3b6149e4e57fdb590d2b454f57b4dfae9bf7e2
Felipe Colletti <colletti.felipe@gmail.com>
2024-09-26 19:37:01 -0300

    Commit

--- ScreenSound/Banco/Connection.cs
+++ ScreenSound/Banco/Connection.cs
@@ -1,15 +1,9 @@
 ﻿using Microsoft.Data.SqlClient;
-using ScreenSound.Modelos;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ScreenSound.Banco;
 internal class Connection
 {
-    private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+    private readonly string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV0;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
     public SqlConnection ObterConexao()
     {
